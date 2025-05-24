97fa8875db37f051436defd33fe2feb8b51a9883
Andriy-Parashchuk <andreiparaschyk1401@gmail.com>
2025-05-24 17:01:15 +0300

    Create models for project
    Add Subject model
    Add Teacher model

--- lms/lms_service/models.py
+++ lms/lms_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True)
+
+
